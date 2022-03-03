51c346a94cd1a7b8ed21a5041ab38d5e45128463
Vitor.Gomes <santosgomesv@gmail.com>
2022-03-03 12:42:26 -0300

    pagina principal

---
+++ usuarios/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.principal, name = 'principal'),
+    path('cadastro/', views.cadastro, name = 'cadastro'),
+    path('login/', views.login, name = 'login'),
+    path('valida_cadastro/', views.valida_cadastro, name = 'valida_cadastro'),
+] 
